b4f5124f10105d1e6fa12edc3eb8d0972e2aa9bd
DmitryZlobec <zlobec@gmail.com>
2016-07-26 22:50:54 +0300

    C++ realisation MQTT connections

---
+++ TMQTTSNMPHandler.cpp
@@ -0,0 +1,107 @@
+//
+// Created by zlobec on 26.07.16.
+//
+
+#include "TMQTTSNMPHandler.h"
+static u_long hashData(const char *str)
+{
+    u_long hash = 5381;
+    int c;
+    while (c=*str++)
+        hash =((hash << 5) + hash) + c;
+
+    return hash;
+
+}
+
+TMQTTSNMPHandler::TMQTTSNMPHandler(const TMQTTSNMPHandler::TConfig &config,
+                                   netSnmpMQTT::netSnmpMQTTTable &netSnmpMQTTTable) : TMQTTWrapper(config),netSnmpTable (netSnmpMQTTTable)
+{
+
+    Connect();
+
+}
+
+
+
+
+void TMQTTSNMPHandler::OnConnect(int rc) {
+
+    DEBUGMSGTL(("verbose", "Connected with code: %d\n", rc));
+
+    if(rc == 0) {
+        string prefix = string("/devices/") + MQTTConfig.Id + "/";
+        // Meta
+        Publish(NULL, prefix + "meta/name", "SNMP", 0, false);
+    }
+
+}
+
+void TMQTTSNMPHandler::OnSubscribe(int mid, int qos_count, const int *granted_qos) {
+
+    DEBUGMSGTL(("verbose", "Subscription succeeded.\n"));
+
+}
+
+void TMQTTSNMPHandler::OnMessage(const struct mosquitto_message *message) {
+
+    string topic = message->topic;
+    string payload = static_cast<const char *>(message->payload);
+
+
+    const vector<string>& tokens = StringSplit(topic, '/');
+    if (tokens.size() < 5) {
+
+        DEBUGMSGTL(("snmp-mqtt-wb", "not enough topic sections to care: %s is %d\n", topic, tokens.size()));
+        return;
+
+    }
+
+    if ( tokens[1] != "devices" || tokens[0] != "")
+    {
+
+        DEBUGMSGTL(("snmp-mqtt-wb", "no devices topic: %s \n", topic));
+        return;
+
+    }
+
+    string rowName = tokens[2] + "/" + tokens[4];
+    unsigned long int sec = (unsigned long int)time(NULL);
+
+    if (tokens[5] == "meta" && tokens[6] == "type") {
+
+        netSnmpTable.addUpdateType(hashData(rowName.c_str()), rowName.c_str(), payload.c_str());
+        return;
+    }
+
+    if (tokens[5] == "meta" && tokens[6] == "order") {
+
+        netSnmpTable.addupdateOrder(hashData(rowName.c_str()), payload.c_str());
+        return;
+    }
+
+    if (tokens[5] == "meta" && tokens[6] == "readonly") {
+
+        netSnmpTable.addUpdateReadonly(hashData(rowName.c_str()), payload.c_str());
+        return;
+    }
+
+    if (tokens[5] == "meta" && tokens[6] == "ts") {
+
+        netSnmpTable.addUpdateTs(hashData(rowName.c_str()), payload.c_str());
+        return;
+    }
+    if (tokens[3] == "controls" && tokens.size() == 5) {
+        std::stringstream ss;
+        ss << sec;
+        std::string ts = ss.str();
+        DEBUGMSGTL(("snmp-mqtt-wb", "Create/Update row: %s with value1: %s, timestamp: %d\n",rowName, payload, sec));
+        netSnmpTable.addUpdate(hashData(rowName.c_str()),rowName.c_str(), payload.c_str(), ts.c_str());
+        return;
+    }
+}
+
+TMQTTSNMPHandler::~TMQTTSNMPHandler() {
+
+}
+
---
+++ TMQTTSNMPHandler.h
@@ -0,0 +1,39 @@
+//
+// Created by zlobec on 26.07.16.
+//
+
+#ifndef WB_SNMP_TMQTTSNMPHANDLER_H
+#define WB_SNMP_TMQTTSNMPHANDLER_H
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+
+
+#include <mosquittopp.h>
+#include <wbmqtt/utils.h>
+#include <wbmqtt/mqtt_wrapper.h>
+#include<chrono>
+#include<thread>
+
+#include <net-snmp/net-snmp-config.h>
+#include <net-snmp/net-snmp-includes.h>
+#include <net-snmp/agent/net-snmp-agent-includes.h>
+#include "netSnmpMQTTTable.h"
+
+class TMQTTSNMPHandler  : public TMQTTWrapper {
+public:
+    TMQTTSNMPHandler(const TMQTTSNMPHandler::TConfig &config, netSnmpMQTT::netSnmpMQTTTable &netSnmpMQTTTable);
+    ~TMQTTSNMPHandler();
+    void OnConnect(int rc);
+    void OnMessage(const struct mosquitto_message *message);
+    void OnSubscribe(int mid, int qos_count, const int *granted_qos);
+private:
+    netSnmpMQTT::netSnmpMQTTTable netSnmpTable;
+};
+
+
+#endif //WB_SNMP_TMQTTSNMPHANDLER_H
